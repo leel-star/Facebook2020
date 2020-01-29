fb2ce4b76d127cb55da56f52b01ea25ac02975b5
Matt Sutton <mattsuttondev@gmail.com>
2020-01-28 20:52:38 -0500

    Added header

--- FB2020/MainController.swift
+++ FB2020/MainController.swift
@@ -34,13 +34,91 @@ class PostCell: LBTAListCell<String> {
     }
 }
 
-class MainController: LBTAListController<PostCell, String>, UICollectionViewDelegateFlowLayout {
+class StoryHeader: UICollectionReusableView {
+    
+    let storiesController = StoriesController(scrollDirection: .horizontal)
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .yellow
+        
+        stack(storiesController.view)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+}
+
+class StoryPhotoCell: LBTAListCell<String> {
+    
+    let imageView = UIImageView(image: nil, contentMode: .scaleAspectFill)
+    let nameLabel = UILabel(text: "Mountains", font: .boldSystemFont(ofSize: 14), textColor: .white)
+    
+    let gradientLayer = CAGradientLayer()
+    
+    override var item: String! {
+        didSet {
+            imageView.image = UIImage(named: item)
+        }
+    }
+    
+    override func setupViews() {
+        imageView.layer.cornerRadius = 10
+        
+        stack(imageView)
+        setupGradientLayer()
+        stack(UIView(), nameLabel).withMargins(.allSides(8))
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        gradientLayer.frame = bounds
+    }
+    
+    fileprivate func setupGradientLayer() {
+        gradientLayer.colors = [UIColor.clear.cgColor, UIColor.black.cgColor]
+        gradientLayer.locations = [0.7, 1.1]
+        layer.cornerRadius = 10
+        clipsToBounds = true
+        
+        layer.addSublayer(gradientLayer)
+    }
+}
+
+class StoriesController: LBTAListController<StoryPhotoCell, String>, UICollectionViewDelegateFlowLayout {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.items = ["image6", "image5", "image4", "image3", "image2", "image1"]
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return .init(width: 100, height: view.frame.height - 24)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return .init(top: 0, left: 12, bottom: 0, right: 12)
+    }
+}
+
+class MainController: LBTAListHeaderController<PostCell, String, StoryHeader>, UICollectionViewDelegateFlowLayout {
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         collectionView.backgroundColor = .init(white: 0.9, alpha: 1)
         self.items = ["hello", "World", "1", "2"]
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+        return .init(width: 0, height: 200)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return .init(top: 12, left: 0, bottom: 0, right: 0)
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
